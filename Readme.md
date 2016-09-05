27a8d292eebebb5168cd70cc64fd5fd5f6db8cdb
Steven Bock <steven@stevenbock.me>
2016-09-05 16:54:22 -0700

    Added Readme and setup script

---
+++ Readme.md
@@ -0,0 +1,4 @@
+LinkScanner
+===========
+
+This is a Python package for retrieving all hyperlinks on a webpage.
---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(name='LinkScanner',
+      version='1.0',
+      description='HTTP Link Scanner for Python',
+      author='Steven Bock',
+      author_email='steven@stevenbock.me',
+      url='https://github.com/dabockster/LinkScanner',
+      packages=[],
+      )
