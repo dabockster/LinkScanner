2c401a13a8bebf2337a972bfe0f77f5f982db3e6
Steven Bock <steven@stevenbock.me>
2016-09-07 09:46:58 -0700

    Very basic functionality is done. Stupidly reads in the entire lines containing links (even if they're under a thousand div tags).

--- LinkScanner/LinkScanner.py
+++ LinkScanner/LinkScanner.py
@@ -1,7 +1,24 @@
 import urllib.request
+import io
 
 def main():
-    website = urllib.request.urlopen()
+    # Ask user for URL
+    url = input("Enter a URL: ")
 
-if __name__ = "__main__":
+    # Grab the page located at the specified URL
+    website = urllib.request.urlopen(url, data = None)
+
+    # Format the webpage into something human readable
+    document = io.TextIOWrapper(website, encoding = 'utf-8')
+    text = document.read()
+
+    # Split the HTML file line by line
+    lines = text.split("qr/\R/");
+
+    # output links line by line
+    for i in lines:
+        if "a href" in i:
+            print(i)
+
+if __name__ == "__main__":
     main()
